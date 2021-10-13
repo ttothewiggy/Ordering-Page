f825a0de42751db3ec3550a9cc3818fd0cef402a
ttothewiggy <76453825+ttothewiggy@users.noreply.github.com>
2021-10-13 19:04:29 +1300

    Introduce flexbox

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ function myFunction() {
   }
   
 */
+
+const userInput = parseInt(prompt("Enter a number larger than 1: "));
+alert("Your number is " + userInput);
+
+for (let i = 1; i <= userInput; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0){
+        console.log("Fizz");
+    } else if (i % 5 === 0){
+        console.log("Buzz");
+      }else {
+        console.log(i);
+    }
+    
+}
+
+//console.log(lastletter("hello"));
+
