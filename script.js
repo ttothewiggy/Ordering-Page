1a6b152be3a9192bbe4d58898b8fad6af0039057
ttothewiggy <76453825+ttothewiggy@users.noreply.github.com>
2021-10-16 14:41:06 +1300

    Wrote JS for form send and reset

--- script.js
+++ script.js
@@ -21,21 +21,68 @@ function myFunction() {
   
 */
 
-const userInput = parseInt(prompt("Enter a number larger than 1: "));
-alert("Your number is " + userInput);
 
-for (let i = 1; i <= userInput; i++){
-    if (i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz");
-    } else if (i % 3 === 0){
-        console.log("Fizz");
-    } else if (i % 5 === 0){
-        console.log("Buzz");
-      }else {
-        console.log(i);
-    }
-    
+/* form 
+initialising the send and reset buttons*/
+
+const init = function() {
+  document.getElementById("button-cancel").addEventListener("click", reset);
+  document.getElementById("button-send").addEventListener("click", send);
+  
 }
 
-//console.log(lastletter("hello"));
+/* function for reset button */
+
+const reset = function(ev){
+  ev.preventDefault();
+  document.getElementById("form-user").reset();
+}
+
+/* function for send button */
+
+const send = function(ev){
+  //ev.preventDefault();
+  
+
+  let ret = validate();
+
+  if (ret){
+    document.getElementById("form-user").submit();
+
+  } else{
+    let err = document.querySelector(".error");
+    let input = err.querySelector("input");
+    err.setAttribute("data-errormsg", `...Missing ${input.placeholder}`);
+  }
+}
+
+/* function for validation */
+
+
+const validate = function(ev){
+  let valid = false;
+
+  let first = document.getElementById("input-first");
+  if (Selection.selectedIndex === 0){
+    failures.push({input:"input-first", msg:"Please enter a name"})
+  }
+
+
+  let last = document.getElementById("input-last");
+  let type = document.getElementById("input-type");
+  let construction = document.getElementById("input-construction");
+  let length = document.getElementById("input-length");
+  let width = document.getElementById("input-width");
+  let thickness = document.getElementById("input-thickness");
+  let colour = document.getElementById("input-colour");
+  let finish = document.getElementById("input-finish");
+
+  return valid;
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', init);
+
 
